b5e8f3fa186919602a39b9c89bbd5ef49eb18171
Tiger He <tiger.he@data.world>
2018-09-25 16:40:02 -0500

    Adding crude ability to remove all metrics

--- contentScript.js
+++ contentScript.js
@@ -1,12 +1,22 @@
 'use strict';
 
 var removeMetrics = () => {
-  for(var i = document.links.length - 1; i >= 0; i--) {
+  for (var i = document.links.length - 1; i >= 0; i--) {
     if (document.links[i].href.includes('liked_by') ||
         document.links[i].href.includes('following') ||
         document.links[i].href.includes('followers')) {
       document.links[i].parentNode.removeChild(document.links[i]);
     }
+  }
+
+  var profilePhotoOverlays = document.getElementsByClassName("_6S0lP");
+  if (profilePhotoOverlays.length && profilePhotoOverlays[0].childNodes.length) {
+    profilePhotoOverlays[0].removeChild(profilePhotoOverlays[0].childNodes[0]);
+  }
+
+  var popup = document.getElementsByClassName("zV_Nj kCcVy");
+  if (popup.length) {
+    popup[0].parentNode.removeChild(popup[0]);
   }
 }
 
