d7b62f4909f37db761d4820b8200bfdc698b26e7
Tiger He <tigerhe7@gmail.com>
2018-09-25 20:15:35 -0500

    Removing all like/follower metrics

--- contentScript.js
+++ contentScript.js
@@ -1,27 +1,18 @@
 'use strict';
 
 var removeMetrics = () => {
-  for(var i = document.links.length - 1; i >= 0; --i) {
-    if (document.links[i].href.includes('liked_by') ||
-        document.links[i].href.includes('following') ||
-        document.links[i].href.includes('followers')) {
-      document.links[i].parentNode.removeChild(document.links[i]);
+  var links = document.links;
+  for (var i = links.length - 1; i >= 0; --i) {
+    if (links[i].href.includes('liked_by') ||
+        links[i].href.includes('following') ||
+        links[i].href.includes('followers')) {
+      links[i].parentNode.removeChild(links[i]);
     }
-  }
-
-  var profilePhotoOverlays = document.getElementsByClassName("_6S0lP");
-  if (profilePhotoOverlays.length && profilePhotoOverlays[0].childNodes.length) {
-    profilePhotoOverlays[0].removeChild(profilePhotoOverlays[0].childNodes[0]);
-  }
-
-  var popup = document.getElementsByClassName("zV_Nj kCcVy");
-  if (popup.length) {
-    popup[0].parentNode.removeChild(popup[0]);
   }
 }
 
-// There should definitely be a better way to do this - waiting for components
-//   to load or watching network requests... but eh it works for now...
+// TODO: Should wait for components to load or watch network requests.
+// works for now tho
 setInterval(function() {
   removeMetrics();
 }, 100);
